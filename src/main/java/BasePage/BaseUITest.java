db66eae3827f1e76903fce960c2a843b2310036b
michalsl <leszek.michalski@contractors.roche.com>
2023-11-06 18:07:48 +0100

    Update

--- src/main/java/BasePage/BaseUITest.java
+++ src/main/java/BasePage/BaseUITest.java
@@ -1,7 +1,6 @@
 package BasePage;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
